11022804bf970f780719d115bc046592fc1687f9
kim-jin-seop <tjq2702@naver.com>
2022-05-15 02:15:18 +0900

    JumpToPython 세션2 연습문제
    파이썬 프로그래밍의 기초, 자료형과 관련한 이론 학습 후 연습문제 풀이

---
+++ 1.JumpToPython/session2/q1.py
@@ -0,0 +1,9 @@
+# 홍길동 씨의 과목별 점수는 다음과 같다. 홍길동 씨의 평균 점수를 구해 보자.
+# 과목	점수
+# 국어	80
+# 영어	75
+# 수학	55
+
+korean, english, math = 80,75,55
+avg = (korean + english + math) / 3
+print(avg)
---
+++ 1.JumpToPython/session2/q7.py
@@ -0,0 +1,4 @@
+# ['Life', 'is', 'too', 'short'] 리스트를 Life is too short 문자열로 만들어 출력해 보자.
+list = ['Life', 'is', 'too', 'short']
+result = " ".join(list)
+print(result)
